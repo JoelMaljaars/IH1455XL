6a20942ed43d340c2427a56c0218ac2ea6d1f4a0
JeroenRMU <jeroendewolf2006@gmail.com>
2022-12-14 21:42:13 +0100

    14-12 jeroen

--- lib/login.dart
+++ lib/login.dart
@@ -29,7 +29,7 @@ class LoginScreen extends StatelessWidget {
               Container(
                 margin: EdgeInsets.only(top: 180),
                 child: const Text(
-                  'Deta',
+                  'DeTa',
                   textAlign: TextAlign.center,
                   textScaleFactor: 4.0,
                   style: TextStyle(
